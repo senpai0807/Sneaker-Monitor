29fab7e4a3df8f87d091469f0bb64b1009020050
Brandon <134825293+senpai0807@users.noreply.github.com>
2023-08-14 16:26:07 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const monitorProducts = require('./Monitors/Shopify/monitor');
+const monitor = require('./Monitors/Shopify/checkpoint');
+const monitorSnipes = require('./Monitors/Snipes/snipes');
+const monitorNike = require('./Monitors/Nike/nike');
+const createColorizedLogger = require('./Dependencies/Functions/logger');
+const logger = createColorizedLogger();
+require('./Dependencies/Server/server');
+
+async function main() {
+    monitorProducts().then(async () => {
+      await monitor().then(async () => {
+          await monitorNike()
+        }).catch((err) => {
+            logger.error(err);
+        });
+}).catch((err) => {
+    logger.error(err);
+})};
+
+main();
